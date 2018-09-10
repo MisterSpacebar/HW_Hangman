d8a032e1b9feffdf3168405cfde881932aa607f7
Yibin Wei <yibinwei@live.com>
2018-09-09 22:36:41 -0400

    changed to buttons

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var wordLibrary = ["IMPORTANCE","GODPARENT","CONJUGATED","DUPLICATE","EMBRACING","OVERSTAY","PERSONALITY","SLUMBER","LUMBERJACK","THUNDERCLAP","SUBCATEGORY","FARSIGHTED","BACKGROUND","COPYRIGHT",
                     "DOCUMENTARY","DANGEROUS","GEOPHYSICAL","BIRTHPLACE","CAMPGROUND","COUNTRYSIDE","SPACEFLIGHT","WORKMANSHIP","SHOCKINGLY","ISOGRAM","DOWNSTREAM","ATMOSPHERIC","BANKRUPTCY",
                     "BLACKSMITH","COMPLAIN","HOSPITABLY","BOYFRIEND","CLOTHESPIN","GREYHOUND","INTRODUCE","NIGHTMARE","PATHFINDER","DRAGON","PHLEGMATIC","ULTRASONIC","BINOCULARS","TRAMPOLINE"];
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 var word; //selected word
 var wordToGuess; //array of selected word
 var answerArray = []; //blank array
 var guessCount;
-var keyGuess
+var wordCount;
 
 window.onload = function startGame(){
     hangmanStart();
@@ -17,50 +17,69 @@ window.onload = function startGame(){
 function hangmanStart(){
     word = wordLibrary[randNum(wordLibrary.length)];
     wordToGuess = wordToArray(word);
-    
-    for(var x=0;x<wordToGuess.length;x++){
-        answerArray[x]="_";
-    }
-
+    blankArray(wordToGuess);
     guessCount = word.length;
+    wordCount = word.length;
+}
+
+// function gameLoop(){
+//     while(guessCount>=0){
+//         document.getElementById("wordGame").innerHTML = answerArray.join(" ");
+//         wordCheck();
+//     }
+// }
+
+function blankArray(word){
+    for(var x=0;x<wordToGuess.length;x++){
+    answerArray[x]="_";
+    }
+}
+
+function buttonReturn(button){
+    var buttonAns = button.id;
+    console.log(buttonAns);
+    return buttonAns;
 }
 
 function gameLoop(){
-    while(guessCount>0){
-        document.getElementById("wordGame").innerHTML=answerArray.join(" ");
+    var counter = guessCount;
+    var guess;
+    while(counter>0){
+        document.getElementById("wordGame").innerHTML = answerArray.join(" ");
 
-        var guess = keyGet(document.getElementById("textInput"));
-        // if(guess===null){
-        //     break;
-        // } else {
-        //     for(var i=0; i<wordToGuess.length; i++){
-        //         if(wordToGuess[i]==guess){
-        //             answerArray[i]=guess;
-        //             guessCount--;
-        //         }
-        //     }
-        // }
-        if(wordToGuess.indexOf(guess)>-1){
-            for(var i=0 ; i<wordToGuess.length ; i++){
-                if(wordToGuess[1]==guess){
+        guess = buttonReturn(button);
+
+        if(wordCount==0){
+            alert("You win the game!");
+        } else if(wordToGuess.indexOf(guess)>-1){
+            for(var i=0; i<wordToGuess.length; i++){
+                if(wordToGuess[i]==guess){
                     answerArray[i]=guess;
-                    guessCount--;
+                    wordCount--;
+                    counter--;
                 }
             }
+        } else {
+            guessCount--;
+            counter--;
         }
     }
-    document.getElementById("countGuesses").innerHTML=guessCount+" guesses left";
 }
 
-document.onkeypress = function keyGet(input){
-    var keyload = input.toLowerCase;
+// function wordCheck(){
+//     var guess = buttonReturn(button);
 
-    if(alphabet.indexOf(keyload)>-1){
-        keyGuess = keyload.toUpperCase;
-    }
-
-    return keyGuess;
-}
+//     if(wordToGuess.indexOf(guess)>-1){
+//         for(var i=0; wordToGuess.length; i++){
+//             if(wordToGuess[i]==guess){
+//                 answerArray[i]=guess;
+//             }
+//         }
+//     } else {
+//         guessCount--;
+//     }
+//     document.getElementById("countGuesses").innerHTML=guessCount+" guesses left";
+// }
 
 function randNum(upperBound){
     var result = Math.floor((Math.random()*upperBound));
